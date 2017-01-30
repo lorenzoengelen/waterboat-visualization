4c8247ae6ca7e73318786238e651983bdd74fc60
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-01-30 16:08:47 +0100

    specific production requirements

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
-  
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+  app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
   app.use(express.static('dist'));
   app.get('*', (req, res) => {
