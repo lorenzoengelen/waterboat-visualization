d91956b7a1a19794e49f89842f5812fff5922493
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-06 09:51:00 +0100

    index

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Gmap from './components/Gmap.jsx';
+
+const App = () => {
+  return (
+    <Gmap />
+  );
+};
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
