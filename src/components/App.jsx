664a0183d637c11df3032334e8409117812cd02d
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-06 12:06:18 +0100

    app mod

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>This is the app</h1>
+      </div>
+    );
+  }
+};
+
+export default App;
