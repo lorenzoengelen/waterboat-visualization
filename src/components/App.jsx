d579021aa421017259f6c96e4a393add131af0bd
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-06 12:16:45 +0100

    basic dashboard layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import 'bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>This is the app</h1>
+      <div className='app'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-xs-8'>
+              <div className='row'>
+                <h1>Google map here</h1>
+              </div>
+              <div className='row'>
+                <h1>Timeline here</h1>
+              </div>
+            </div>
+            <div className='col-xs-4'>
+              <h1>col-xs-5</h1>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
