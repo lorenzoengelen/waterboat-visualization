6dd385d0155a6721f8c157dd7aeba1427148a5f0
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-08 17:20:23 +0100

    render the log track of the waterboats for 2 january refactor needed

--- src/components/Polylines.jsx
+++ src/components/Polylines.jsx
@@ -1,5 +1,12 @@
 import React, { Component, PropTypes as T } from 'react';
 
+import data1 from '../../json/waterboat1/history.json';
+import data10 from '../../json/waterboat10/history.json';
+import data12 from '../../json/waterboat12/history.json';
+import data15 from '../../json/waterboat15/history.json';
+import data2 from '../../json/waterboat2/history.json';
+import data5 from '../../json/waterboat5/history.json';
+
 class Polylines extends Component {
   componentDidUpdate(prevProps) {
     if (this.props.map !== prevProps.map) {
@@ -8,38 +15,147 @@ class Polylines extends Component {
   }
 
   renderLine() {
-    console.log(this.props);
     let {map, google} = this.props;
+    
+    // 1
+    let coordinates1 = [];
+    data1.forEach(log => {
+      if (log.timeLastUpdate > 1483398000000) {
+        return;
+      }
+      coordinates1.push({
+        lat: log.location.latitude,
+        lng: log.location.longitude
+      });
+    });
 
-    let coordinates = [
-      {lat: 51.90401, lng: 4.43735},
-      {lat: 51.904, lng: 4.43733},
-      {lat: 51.90399, lng: 4.43734},
-      {lat: 51.90399, lng: 4.43737},
-      {lat: 51.90401, lng: 4.43726},
-      {lat: 51.90369, lng: 4.43662},
-      {lat: 51.90354, lng: 4.43421},
-      {lat: 51.90295, lng: 4.43},
-      {lat: 51.90216, lng: 4.42613},
-      {lat: 51.90127, lng: 4.42158},
-      {lat: 51.90059, lng: 4.41771},
-      {lat: 51.8997, lng: 4.41321},
-      {lat: 51.89887, lng: 4.40944},
-      {lat: 51.89797, lng: 4.40506},
-      {lat: 51.89716, lng: 4.40134},
-      {lat: 51.8965, lng: 4.39681},
-      {lat: 51.89608, lng: 4.39299},
-    ];
+    this.line1 = new google.maps.Polyline({
+      path: coordinates1,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1,
+      strokeWeight: 1,
+    });
 
-    this.line = new google.maps.Polyline({
-      path: coordinates,
+    this.line1.setMap(map);
+    // 1
+
+    // 2
+    
+    // 1
+    let coordinates2 = [];
+    data10.forEach(log => {
+      if (log.timeLastUpdate > 1483398000000) {
+        return;
+      }
+      coordinates2.push({
+        lat: log.location.latitude,
+        lng: log.location.longitude
+      });
+    });
+
+    this.line2 = new google.maps.Polyline({
+      path: coordinates2,
+      geodesic: true,
+      strokeColor: '#FF00FF',
+      strokeOpacity: 1,
+      strokeWeight: 1,
+    });
+
+    this.line2.setMap(map);
+    // 2
+
+    // 3
+    let coordinates3 = [];
+    data12.forEach(log => {
+      if (log.timeLastUpdate > 1483398000000) {
+        return;
+      }
+      coordinates3.push({
+        lat: log.location.latitude,
+        lng: log.location.longitude
+      });
+    });
+
+    this.line3 = new google.maps.Polyline({
+      path: coordinates3,
+      geodesic: true,
+      strokeColor: '#00FF00',
+      strokeOpacity: 1,
+      strokeWeight: 1,
+    });
+
+    this.line3.setMap(map);
+    // 3
+
+    // 4
+    let coordinates4 = [];
+    data15.forEach(log => {
+      if (log.timeLastUpdate > 1483398000000) {
+        return;
+      }
+      coordinates4.push({
+        lat: log.location.latitude,
+        lng: log.location.longitude
+      });
+    });
+
+    this.line4 = new google.maps.Polyline({
+      path: coordinates4,
+      geodesic: true,
+      strokeColor: '#FFFF00',
+      strokeOpacity: 1,
+      strokeWeight: 1,
+    });
+
+    this.line4.setMap(map);
+    // 4
+
+    // 5
+    let coordinates5 = [];
+    data2.forEach(log => {
+      if (log.timeLastUpdate > 1483398000000) {
+        return;
+      }
+      coordinates5.push({
+        lat: log.location.latitude,
+        lng: log.location.longitude
+      });
+    });
+
+    this.line5 = new google.maps.Polyline({
+      path: coordinates5,
+      geodesic: true,
+      strokeColor: '#0000FF',
+      strokeOpacity: 1,
+      strokeWeight: 1,
+    });
+
+    this.line5.setMap(map);
+    // 5
+
+    // 6
+    let coordinates6 = [];
+    data5.forEach(log => {
+      if (log.timeLastUpdate > 1483398000000) {
+        return;
+      }
+      coordinates6.push({
+        lat: log.location.latitude,
+        lng: log.location.longitude
+      });
+    });
+
+    this.line6 = new google.maps.Polyline({
+      path: coordinates6,
       geodesic: true,
       strokeColor: '#000000',
       strokeOpacity: 1,
-      strokeWeight: 2,
+      strokeWeight: 1,
     });
 
-    this.line.setMap(map);
+    this.line6.setMap(map);
+    // 6
   }
 
   render() {
