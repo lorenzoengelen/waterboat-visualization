fa191adadf7cadcfce99781897124f036bf2bffc
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-08 14:11:46 +0100

    created polylines component

---
+++ src/components/Polylines.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes as T } from 'react';
+
+class Polylines extends Component {
+  componentDidUpdate(prevProps) {
+    
+  }
+
+  render() {
+    return null;
+  }
+};
+
+export default Polylines;
