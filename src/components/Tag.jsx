faaa04f44aeae9e54b68c50f931b1ac6b193dd5a
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-08 13:54:01 +0100

    created a Tag component equal to an annotation point

---
+++ src/components/Tag.jsx
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes as T } from 'react';
+
+class Tag extends Component {
+  componentDidUpdate(prevProps) {
+    if ((this.props.map !== prevProps.map) || (this.props.position !== prevProps.position)) {
+      // the relevant props have changed
+      this.renderTag();
+    }
+  }
+
+  renderTag() {
+    let {map, google, position, mapCenter} = this.props;
+
+    let pos = position || mapCenter;
+    position = new google.maps.LatLng(pos.lat, pos.lng);
+
+    const pref = {
+      map: map,
+      position: position
+    };
+
+    this.tag = new google.maps.Marker(pref);
+  }
+
+  render() {
+    return null;
+  }
+};
+
+Tag.propTypes = {
+  position: T.object,
+  map: T.object
+};
+
+export default Tag;
