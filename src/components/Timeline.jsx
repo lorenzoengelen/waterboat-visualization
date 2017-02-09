388dbce9eb4727c2131b6dc275151fcf632f09f6
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-09 16:14:14 +0100

    created a timeline componenet

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Timeline extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Is this a F* timeline?</h1>
+      </div>
+    );
+  }
+};
+
+export default Timeline;
