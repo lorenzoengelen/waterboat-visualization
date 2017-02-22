666a31cdd3cb545077b27c7452e20c0c7de49fc9
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-02-22 14:25:33 +0100

    demo timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,11 @@
 import React, { Component, PropTypes as T } from 'react';
 import ReactDom from 'react-dom';
 import * as d3 from 'd3';
+import * as vis from 'vis';
+import moment from 'moment';
 
 // import TL from './d3Timeline.jsx';
-import TimelineChart from 'd3-timeline-chart';
+// import TimelineChart from 'd3-timeline-chart';
 
 class Timeline extends Component {
   constructor(props) {
@@ -12,65 +14,90 @@ class Timeline extends Component {
 
   componentDidMount() {
     this.el = ReactDom.findDOMNode(this);
-    // this.tl = new TL();
-    // this.tl.create(this.el, {
-    //   width: '100%',
-    //   height: '40vh'
-    // }, this.getChartState());
+    
+    var now = moment().minutes(0).seconds(0).milliseconds(0);
+    var itemCount = 60;
 
-    this.data = [{
-      label: 'Name',
-      data: [{
-        type: TimelineChart.TYPE.POINT,
-        at: new Date([2015, 1, 11])
-      }, 
+    // create a data set with groups
+    var groups = new vis.DataSet();
+
+    groups.add([
       {
-        type: TimelineChart.TYPE.POINT,
-        at: new Date([2015, 1, 15])
+        id: 1,
+        content: "Lee",
+        nestedGroups: [11,12,13]
       },
       {
-        type: TimelineChart.TYPE.POINT,
-        at: new Date([2015, 3, 10])
+        id: 2,
+        content: "invisible group",
+        visible: false
       },
       {
-        label: 'I\'m a label',
-        type: TimelineChart.TYPE.INTERVAL,
-        from: new Date([2015, 2, 1]),
-        to: new Date([2015, 3, 1])
+        id: 3,
+        content: "John",
+        nestedGroups: [14],
+        showNested: false
       },
       {
-        type: TimelineChart.TYPE.POINT,
-        at: new Date([2015, 6, 1])
+        id: 4,
+        content: "Alson"
+      },
+    ]);
+
+    groups.add([
+      {
+        id: 11,
+        content: "cook",
       },
       {
-        type: TimelineChart.TYPE.POINT,
-        at: new Date([2015, 7, 1]),
-        customClass: 'custom-class'
-      }]
-    }];
+        id: 12,
+        content: "shop",
+      },
+      {
+        id: 13,
+        content: "clean house",
+      },
+      {
+        id: 14,
+        content: "wash dishes",
+      }
+    ]);
 
+    // create a dataset with items
+    var items = new vis.DataSet();
+    var groupIds = groups.getIds();
+    var types = [ 'box', 'point', 'range', 'background']
+    for (var i = 0; i < itemCount; i++) {
+      var start = now.clone().add(Math.random() * 200, 'hours');
+      var end = start.clone().add(2, 'hours');
+      var randomGroupId = groupIds[Math.floor(Math.random() * groupIds.length)];
+      var type = types[Math.floor(4 * Math.random())]
 
+      items.add({
+        id: i,
+        group: randomGroupId,
+        content: 'item ' + i,
+        start: start,
+        end: end,
+        type: type
+      });
+    }
 
-    this.timeline = new TimelineChart(this.el, this.data, {
-      // tip: function(d) {
-      //   return d.at || `${d.from}<br>${d.to}`;
-      // }
-    });
+    // create visualization
+    var container = this.el; // document.getElementById('visualization');
+    var options = {
+      groupOrder: 'content'  // groupOrder can be a property name or a sorting function
+    };
 
-    console.log(this.timeline);
+    var timeline = new vis.Timeline(container, items, groups, options); 
   }
 
   componentDidUpdate() {
-    this.timeline = new TL(this.el, this.data, {
-          // tip: function(d) {
-          //   return d.at || `${d.from}<br>${d.to}`;
-          // }
-    });
-    // console.log(this.timeline);
+    
   }
 
   componentWillUnmount() {
-    // this.tl.destroy(this.el);
+    
   }
 
   getChartState() {
