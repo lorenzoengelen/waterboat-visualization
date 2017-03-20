6df8ab83e49487a347da7237722e3a418a0f40a4
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-03-20 21:42:36 +0100

    created a folder for server side stuff

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.get('/hello', (req, res) => {
+  res.send('eak');
+});
+
 if (process.env.NODE_ENV !== 'production') {
   const webpackMiddleware = require('webpack-dev-middleware');
   const webpack = require('webpack');
   const webpackConfig = require('./webpack.config.js');
   app.use(webpackMiddleware(webpack(webpackConfig)));
+
 } else {
   app.use(express.static('dist'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
+
 }
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
